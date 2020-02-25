f61d20b8d82e3711170866664bfc608645d3202f
MateoLeon <mateoleon127@gmail.com>
2020-02-24 21:44:09 -0500

    Creación de backend, rutas de revisión y botiquin y llamados a mongodb, prettier, eslint y npm init

---
+++ db.js
@@ -0,0 +1,103 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+let db;
+
+// Database Name
+const dbName = "rucAssistantDB";
+
+// Create a new MongoClient
+
+const init = () =>
+  MongoClient.connect(process.env.MONGODB_URI || url, {
+    useNewUrlParser: true,
+  }).then(client => {
+    db = client.db(dbName);
+  });
+
+const insertOneDoc = (doc, dbCollection) => {
+  const collection = db.collection(dbCollection);
+  return collection.insertOne(doc);
+};
+
+const insertManyDocs = (docs, dbCollection) => {
+  const collection = db.collection(dbCollection);
+  return collection.insertMany(docs);
+};
+
+const getDocs = dbCollection => {
+  const collection = db.collection(dbCollection);
+  return collection.find({}).toArray();
+};
+
+const getDocById = (id, dbCollection) => {
+  const collection = db.collection(dbCollection);
+  return collection
+    .find({
+      _id: ObjectId(id),
+    })
+    .toArray();
+};
+
+const updateDoc = (id, object, dbCollection) => {
+  const collection = db.collection(dbCollection);
+  return collection.replaceOne(
+    {
+      _id: ObjectId(id),
+    },
+    object,
+  );
+};
+
+const getWithJoin = (
+  dbcollection,
+  fromCollection,
+  localField,
+  foreingField,
+  asName,
+) => {
+  const collection = db.collection(dbcollection);
+  return collection
+    .aggregate([
+      { $unwind: "$" + localField },
+      {
+        $lookup: {
+          from: fromCollection,
+          localField: localField,
+          foreingField: foreingField,
+          as: asName,
+        },
+      },
+      { $unwind: "$" + asName },
+      {
+        $group: {
+          _id: "$_id",
+          products: { $push: "$" + localField },
+          productObjects: { $push: "$" + asName },
+        },
+      },
+    ])
+    .toArray();
+};
+
+const pushRevision = (id, revisionId) => {
+  const collection = db.collection("botiquin");
+  return collection.update(
+    { _id: ObjectId(id) },
+    { $push: { revision_id: revisionId } },
+    { upsert: true },
+  );
+};
+
+module.exports = {
+  init,
+  insertOneDoc,
+  insertManyDocs,
+  getDocs,
+  getDocById,
+  updateDoc,
+  getWithJoin,
+  pushRevision,
+};
---
+++ routes/revision.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+const { insertOneDoc, pushRevision } = require("../db");
+
+router.post("/:idBotiquin", (req, res) => {
+  const botiquinId = req.params.idBotiquin;
+  const object = req.body;
+  insertOneDoc(object, "revision").then(response => {
+    if (response.insertedCount == 1) {
+      pushRevision(botiquinId, response.ops[0]._id).then(doc => {
+        res.send(doc);
+      });
+    }
+  });
+});
+
+module.exports = router;
