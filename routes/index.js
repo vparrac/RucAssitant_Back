6b38d72e6e233fc1e7a130d815150d01bcdfa181
unknown <v25a07@gmail.com>
2020-03-01 09:25:20 -0500

    Inicio cambios para unir todo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("../public/views/index.ejs");
 });
 
 module.exports = router;
